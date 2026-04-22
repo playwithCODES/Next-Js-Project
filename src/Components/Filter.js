519f8b990827338834c15eb14533f2da9ee7782d
playwithCODES <omprakashrajbanshi627@gmail.com>
2026-04-22 08:32:32 +0545

    push logics of filter and reset of products

--- src/Components/Filter.js
+++ src/Components/Filter.js
@@ -2,6 +2,7 @@
 
 import { useState } from "react";
 import { useRouter } from "next/navigation";
+import { PRODUCTS_ROUTE } from "@/Constants/routes";
 
 const DEFAULT_SORT = JSON.stringify({ createdAt: -1 });
 const DEFAULT_MIN_PRICE = 0;
@@ -13,21 +14,57 @@ const ProductsFilter = () => {
   const [sort, setSort] = useState(DEFAULT_SORT);
   const [minPrice, setMinPrice] = useState(DEFAULT_MIN_PRICE);
   const [maxPrice, setMaxPrice] = useState(DEFAULT_MAX_PRICE);
+  const [category, setCategory] = useState("");
+  const [name, setName] = useState("");
+  const [brand, setBrand] = useState([]);
 
   function filterProducts() {
-    const params=new URLSearchParams();
+    const params = new URLSearchParams();
     params.set("sort", sort);
     params.set("minPrice", minPrice);
     params.set("maxPrice", maxPrice);
+    params.set("category", category);
+    params.set("name", name);
+    params.set("brand", brand.join(","));
 
     router.push(`?${params.toString()}`);
   }
+
+  function resetFilters() {
+    setSort(DEFAULT_SORT);
+    setMinPrice(DEFAULT_MIN_PRICE);
+    setMaxPrice(DEFAULT_MAX_PRICE);
+    setCategory("");
+    setName("");
+    setBrand([]);
+    router.push(PRODUCTS_ROUTE);
+  }
+
+  function handleBrandsFilterChange(brand) {
+    setBrand((prev) =>
+      prev.includes(brand)
+        ? prev.filter((item) => item !== brand)
+        : [...prev, brand],
+    );
+  }
   return (
-    <aside className="shadow-md py-8 px-6">
+    <aside className="shadow-md py-2 px-5">
       <h3 className="text-xl font-semibold text-blue-600">Filter Products</h3>
 
       <div className="py-3">
-        <h4 className="mb-2">Sort By</h4>
+        <h4 className="mb-1 font-semibold">Search</h4>
+        <input
+          name="name"
+          value={name}
+          id="name"
+          className="border border-gray-300 rounded-md w-full px-2 py-1"
+          type="text"
+          onChange={(e) => setName(e.target.value)}
+        />
+      </div>
+
+      <div className="py-2">
+        <h4>Sort By</h4>
         <select
           name="sort"
           id="sort"
@@ -52,9 +89,9 @@ const ProductsFilter = () => {
       </div>
 
       <div className="py-3">
-        <h4 className="mb-2">Price Range</h4>
+        <h4>Price Range</h4>
 
-        <div className="my-5">
+        <div>
           <label htmlFor="minPrice">MinPrice</label>
           <input
             name="min"
@@ -80,12 +117,130 @@ const ProductsFilter = () => {
         </div>
       </div>
 
+      <div>
+        <h4>Category</h4>
+        <select
+          name="category"
+          id="category"
+          onChange={(e) => setCategory(e.target.value)}
+          className="border border-gray-300 rounded-xl w-full px-2 py-1"
+        >
+          <option value="">Select Category</option>
+          <option value="Keyboard">Keyboard</option>
+
+          <option value="Computer">Computer</option>
+          <option value="Smartphones">Smartphones</option>
+          <option value="Laptops">Laptops</option>
+        </select>
+      </div>
+
+      {/* <div className="py-3">
+        <h4 className="mb-2">Brand</h4>
+        <select
+          name="brand"
+          id="brand"
+          onChange={(e) => setBrand(e.target.value)}
+          className="border border-gray-300 rounded-xl w-full px-2 py-1"
+        >
+          <option value="">Select Brand</option>
+          <option value="Apple">Apple</option>
+
+          <option value="OnePlus">OnePlus</option>
+          <option value="Nothing">Nothing</option>
+          <option value="Samsung">Samsung</option>
+          <option value="Dell">Dell</option>
+        </select>
+      </div> */}
+
+      <div className="py-3">
+        <h4 className="mb-2">Brands</h4>
+
+        <div className="flex items-center justify-start gap-2 py-0.5">
+          <input
+            name="Apple"
+            id="Apple"
+            type="checkbox"
+            checked={brand.includes("Apple")}
+            onChange={() => handleBrandsFilterChange("Apple")}
+          />
+          <label htmlFor="Apple" className="text-sm text-grey-700">
+            Apple
+          </label>
+        </div>
+
+        <div className="flex items-center justify-start gap-2 py-0.5">
+          <input
+            name="Samsung"
+            id="Samsung"
+            type="checkbox"
+            checked={brand.includes("Samsung")}
+
+            onChange={() => handleBrandsFilterChange("Samsung")}
+          />
+          <label htmlFor="Samsung" className="text-sm text-gray-700">
+            Samsung
+          </label>
+        </div>
+
+        <div className="flex items-center justify-start gap-2 py-0.5">
+          <input
+            name="Dell"
+            id="Dell"
+            type="checkbox"
+            checked={brand.includes("Dell")}
+            onChange={() => handleBrandsFilterChange("Dell")}
+          />
+          <label htmlFor="Dell" className="text-sm text-gray-700">
+            Dell
+          </label>
+        </div>
+
+        <div className="flex items-center justify-start gap-2 py-0.5">
+          <input
+            type="checkbox"
+            name="Nothing"
+            id="Nothing"
+            checked={brand.includes("Nothing")}
+            onChange={() => handleBrandsFilterChange("Nothing")}
+          />
+          <label htmlFor="Nothing" className="text-sm text-gray-700">
+            Nothing
+          </label>
+        </div>
+
+        <div className="flex items-center justify-start gap-2 py-0.5">
+          <input
+            type="checkbox"
+            name="Oneplus"
+            id="Oneplus"
+            checked={brand.includes("OnePlus")}
+
+            onChange={() => handleBrandsFilterChange("OnePlus")}
+          />
+
+          <label htmlFor="OnePlus" className="text-sm text-gray-700">
+            OnePlus
+          </label>
+        </div>
+      </div>
+
       <div className="py-3">
         <button
           onClick={filterProducts}
-          className="bg-primary text-white rounded-md px-1 py-1"
+          className="bg-primary text-white rounded-md w-full cursor-pointer
+           hover:bg-blue-600
+          px-5 py-1"
         >
           Filter Products
+        </button>
+      </div>
+
+         <div className="py-3">
+        <button
+          onClick={resetFilters}
+          className="bg-red-500 text-white rounded-md px-5 w-full py-1 cursor-pointer hover:bg-red-600"
+        >
+          Reset Filters
         </button>
       </div>
     </aside>
--- src/api/products.js
+++ src/api/products.js
@@ -4,7 +4,10 @@ export const getProducts = async ({searchParams}) => {
   const sort=(await searchParams)?.sort?? "";
   const min=(await searchParams)?.minPrice?? "";
   const max=(await searchParams)?.maxPrice?? "";
+  const category=(await searchParams)?.category?? "";
+  const name=(await searchParams)?.name?? "";
+  const brand=(await searchParams)?.brand?? "";
 
-  const response = await axios.get(`${config.apiUrl}/api/products?sort=${sort}&min=${min}&max=${max}`);
+  const response = await axios.get(`${config.apiUrl}/api/products?sort=${sort}&min=${min}&max=${max}&category=${category}&name=${name}&brand=${brand}`);
   return response.data;
 }
